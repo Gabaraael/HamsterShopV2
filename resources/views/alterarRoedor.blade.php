b65fad9bee16bdc24b16f7778981a7c72f6ed204
VeridianaAmaral <veridiana17timoteo@gmail.com>
2023-05-24 22:24:40 -0400

    Arrumando formularios de alterar

--- resources/views/alterarRoedor.blade.php
+++ resources/views/alterarRoedor.blade.php
@@ -3,10 +3,19 @@
 @section('content')
 
 <body>
-<form action = "" method = "POST">
+<form action = "{{ route('roedor.alterar')}}" method = "POST">
 @csrf
+@method('PUT')
   <div class="form-row">
     <div class="form-group col-md-6">
+    <select name="roedor" class="form-select" aria-label="roedor">
+                                <option selected value="-1">Selecione uma roedor</option>
+                                @foreach ($roedor as $roed)
+                                    <option value="{{ $roed->id }}"
+                                        {{ $roed->id == $roed->roedor ? 'selected' : '' }}>
+                                        {{ $roed->especie }}</option>
+                                @endforeach
+                            </select>
       <label for="inputEspecie4">Especie</label>
       <input type="text" class="form-control" id="inputEspecie4" placeholder="Especie" name = "especie" value= "">
     </div>
