df889cfc1809ab0a7a1042105bc38c40cc5af10a
VeridianaAmaral <veridiana17timoteo@gmail.com>
2023-05-24 22:23:10 -0400

    Adicionando rotas

--- resources/views/cadCategoria.blade.php
+++ resources/views/cadCategoria.blade.php
@@ -7,7 +7,7 @@
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="inputEmail4">Categoria</label>
-      <input type="categoria" class="form-control" id="inputCategoria4" placeholder="Categoria" name = "nome" value="">
+      <input type="text" class="form-control" id="inputCategoria4" placeholder="Categoria" name = "nome" value="">
     </div>
   </div>
   <button type="submit" class="btn btn-primary">Adicionar</button>
