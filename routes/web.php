e619330c91baaabbd96b16d0195e458e159097ca
Gabaraael <Gabriel1998.gp45@gmail.com>
2023-06-22 19:09:07 -0300

    Validando login com middleware

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,16 @@ Route::get('/logout', 'App\Http\Controllers\LoginController@deslogar') -> name('
 Route::get('/', 'App\Http\Controllers\produtoController@listarHome') -> name('home') ;
 
 Route::post('/roedor/cadastro', 'App\Http\Controllers\RoedorController@adicionar') -> middleware('App\Http\Middleware\CheckAuth') ->name("roedor.cadastro");
-Route::get('/roedor/cadastro', 'App\Http\Controllers\RoedorController@pagina')->name("roedor.view");
-Route::get('/roedor/alterar', 'App\Http\Controllers\RoedorController@listar')->name("roedor.listar");
-Route::put('/roedor/alterar', 'App\Http\Controllers\RoedorController@alterar')->name("roedor.alterar");
-Route::delete('/roedor/alterar', 'App\Http\Controllers\RoedorController@deletar')->name("roedor.remover");
+Route::get('/roedor/cadastro', 'App\Http\Controllers\RoedorController@pagina')  -> middleware('App\Http\Middleware\CheckAuth')->name("roedor.view");
+Route::get('/roedor/alterar', 'App\Http\Controllers\RoedorController@listar')  -> middleware('App\Http\Middleware\CheckAuth')->name("roedor.listar");
+Route::put('/roedor/alterar', 'App\Http\Controllers\RoedorController@alterar') -> middleware('App\Http\Middleware\CheckAuth') ->name("roedor.alterar");
+Route::delete('/roedor/alterar', 'App\Http\Controllers\RoedorController@deletar')-> middleware('App\Http\Middleware\CheckAuth')  ->name("roedor.remover");
 
-Route::post('/categoria/cadastro', 'App\Http\Controllers\CategoriaController@adicionar')->name("categoria.cadastro");
-Route::get('/categoria/cadastro', 'App\Http\Controllers\CategoriaController@pagina')->name("categoria.view");
-Route::get('/categoria/alterar', 'App\Http\Controllers\CategoriaController@listar')->name("categoria.listar");
-Route::put('/categoria/alterar', 'App\Http\Controllers\CategoriaController@alterar')->name("categoria.alterar");
-Route::delete('/categoria/alterar', 'App\Http\Controllers\CategoriaController@deletar')->name("categoria.remover");
+Route::post('/categoria/cadastro', 'App\Http\Controllers\CategoriaController@adicionar') -> middleware('App\Http\Middleware\CheckAuth') ->name("categoria.cadastro");
+Route::get('/categoria/cadastro', 'App\Http\Controllers\CategoriaController@pagina') -> middleware('App\Http\Middleware\CheckAuth')->name("categoria.view");
+Route::get('/categoria/alterar', 'App\Http\Controllers\CategoriaController@listar') -> middleware('App\Http\Middleware\CheckAuth') ->name("categoria.listar");
+Route::put('/categoria/alterar', 'App\Http\Controllers\CategoriaController@alterar') -> middleware('App\Http\Middleware\CheckAuth') ->name("categoria.alterar");
+Route::delete('/categoria/alterar', 'App\Http\Controllers\CategoriaController@deletar')-> middleware('App\Http\Middleware\CheckAuth') ->name("categoria.remover");
 
 
 Route::get('/usuario/cadastro', function () {   
@@ -43,12 +43,12 @@ Route::post('/usuario/cadastro', 'App\Http\Controllers\UsuarioController@cadastr
 
 Route::get('/produto/listar/', 'App\Http\Controllers\ProdutoController@listarProdutoPorEspecie') -> name('produto.listar.especie');
 
-Route::get('/produto/cadastro', 'App\Http\Controllers\ProdutoController@pagina')->name("produto.view");
-Route::post('/produto/cadastro', 'App\Http\Controllers\ProdutoController@adicionar')->name("produto.cadastro");
+Route::get('/produto/cadastro', 'App\Http\Controllers\ProdutoController@pagina') -> middleware('App\Http\Middleware\CheckAuth') -> name("produto.view");
+Route::post('/produto/cadastro', 'App\Http\Controllers\ProdutoController@adicionar') -> middleware('App\Http\Middleware\CheckAuth') ->name("produto.cadastro");
 
-Route::get('/produto/alterar', 'App\Http\Controllers\ProdutoController@listar')->name("produto.listar");
-Route::put('/produto/alterar', 'App\Http\Controllers\ProdutoController@alterar')->name("produto.alterar");
-Route::delete('/produto/alterar', 'App\Http\Controllers\ProdutoController@deletar')->name("produto.remover");
+Route::get('/produto/alterar', 'App\Http\Controllers\ProdutoController@listar')  -> middleware('App\Http\Middleware\CheckAuth') ->name("produto.listar");
+Route::put('/produto/alterar', 'App\Http\Controllers\ProdutoController@alterar') -> middleware('App\Http\Middleware\CheckAuth') ->name("produto.alterar");
+Route::delete('/produto/alterar', 'App\Http\Controllers\ProdutoController@deletar')  -> middleware('App\Http\Middleware\CheckAuth') ->name("produto.remover");
 
 
 
